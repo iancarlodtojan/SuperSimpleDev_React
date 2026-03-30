cdd083b5c3c91d22d49bbb562ad19f0b870e05c4
Ian <iankarlodtojan@gmail.com>
2026-03-30 16:37:41 +0800

    Add CheckoutPage component and styles; update routing and HomePage title

--- ecomerce-project/src/App.jsx
+++ ecomerce-project/src/App.jsx
@@ -1,16 +1,13 @@
 import { Routes, Route } from "react-router";
 import HomePage from "./pages/HomePage";
+import CheckoutPage from "./pages/CheckoutPage";
 import "./App.css";
 
 function App() {
   return (
     <Routes>
-      <Route index element={<HomePage />}>
-        {" "}
-      </Route>
-      <Route path="checkout" element={<h1>Checkout</h1>}>
-        {" "}
-      </Route>
+      <Route index element={<HomePage />} />
+      <Route path="checkout" element={<CheckoutPage />} />
     </Routes>
   );
 }
