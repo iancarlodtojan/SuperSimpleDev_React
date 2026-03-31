c3d351e385e23533bca05568a7714fcb2da30ea8
Ian <iankarlodtojan@gmail.com>
2026-03-31 10:40:49 +0800

    fix: correct project name in package files and update API call to use axios

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,13 +1,12 @@
+import axios from "axios";
 import Header from "../components/header";
 import { products } from "../../starting-code/data/products";
 import "./HomePage.css";
 
 function HomePage() {
-  fetch("http://localhost:3000/api/products")
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("Products from API:", data);
-    });
+  axios.get("http://localhost:3000/api/products").then((response) => {
+    console.log(response.data);
+  });
 
   return (
     <>
